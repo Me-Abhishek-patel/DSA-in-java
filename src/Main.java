d4eefb0c1555be1ee51f21cf803fe410717439d4
Abhishek Patel <patelabhishek2007@gmail.com>
2021-05-14 20:17:30 +0530

    Stack Problems 20-28

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("hello World, This is a DSA Project");
         LinkedListProblems linkedListProblems = new LinkedListProblems();
         StackProblems stackProblems = new StackProblems();
-//        llp.problem49();
-        stackProblems.problem11();
+//        linkedListProblems.problem49();
+        stackProblems.problem24and25();
+
     }
 }
 
--- src/StackProblems.java
+++ src/StackProblems.java
@@ -70,11 +70,6 @@ public class StackProblems {
 
     }
 
-    public void problem14() {
-        //implement stack using LL
-        TwoStacks stack = new TwoStacks(100);
-    }
-
     //part of problem11
     private void reverse(Stack<Integer> stack, int data) {
         if (stack.isEmpty()) {
@@ -84,6 +79,81 @@ public class StackProblems {
         int temp = stack.pop();
         reverse(stack, data);
         stack.push(temp);
+    }
+
+    public void problem14() {
+        //implement stack using LL
+        TwoStacks stack = new TwoStacks(100);
+    }
+
+    public void problem22and23() {
+        //Stock span problem
+        System.out.println("problem 22: //Stock span problem.");
+        System.out.println("enter the no. of items in array");
+        int n = sc.nextInt();
+        System.out.println("enter " + n + " items");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        stack.push(0);
+        System.out.print("1 ");
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                stack.pop();
+            }
+            int span = ((stack.isEmpty()) ? i + 1 : i - stack.peek());
+            System.out.print(span + " ");
+            stack.push(i);
+        }
+
+    }
+
+    public void problem24and25() {
+        //Largest area under histogram
+        System.out.println("problem 24 & 25: Largest area under histogram.");
+        System.out.println("enter the no. of items in array");
+        int n = sc.nextInt();
+        System.out.println("enter heights of " + n + " items");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                int temp = stack.pop();
+                int curr = arr[temp] * ((stack.isEmpty()) ? i : (i - stack.peek() - 1));
+                res = Math.max(res, curr);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int tmp = stack.pop();
+            int curr = arr[tmp] * ((stack.isEmpty()) ? n : n - stack.peek() - 1);
+            res = Math.max(res, curr);
+        }
+        System.out.println(res);
+    }
+
+    public void problem28() {
+        //Remove All Adjacent Duplicates
+        System.out.println("problem 28: Remove All Adjacent Duplicates In String.");
+        System.out.println("enter the String");
+        String s = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (stack.peek() == c) {
+                stack.pop();
+            } else stack.push(c);
+        }
+        while (!stack.isEmpty()) sb.append(stack.pop());
+        System.out.println(sb.reverse().toString());
+
     }
 
     //helper methods for LinkedList problems
@@ -114,21 +184,24 @@ public class StackProblems {
         }
 
         void push1(int data) {
-            if (top1 < top2-1) {
+            if (top1 < top2 - 1) {
                 stack[++top1] = data;
             }
         }
+
         void push2(int data) {
-            if (top1 < top2-1) {
+            if (top1 < top2 - 1) {
                 stack[--top2] = data;
             }
         }
-        int pop1(){
-            if(top1>=0) return stack[top1--];
+
+        int pop1() {
+            if (top1 >= 0) return stack[top1--];
             else return -1;
         }
-        int pop2(){
-            if(top2<0) return stack[top2++];
+
+        int pop2() {
+            if (top2 < size) return stack[top2++];
             else return -1;
         }
 
