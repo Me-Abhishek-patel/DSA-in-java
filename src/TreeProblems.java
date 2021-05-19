b2481179f91423ac8f4cceabc550bfc760c679b8
Abhishek Patel <patelabhishek2007@gmail.com>
2021-05-19 14:49:37 +0530

    Tree Problems 10-20

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -146,5 +146,166 @@ public class TreeProblems {
         }
     }
 
+    //problem 11: find height of a binary tree using stack
+    public int heightOfBinaryStack(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Stack<BinaryTreeNode> s = new Stack<>();
+        s.push(root);
+        int depth = 0;
+        BinaryTreeNode prev = null;
+        while (!s.isEmpty()) {
+            BinaryTreeNode curr = s.peek();
+            if (prev == null || prev.left == curr || prev.right == curr) {
+                if (curr.left != null) s.push(curr.left);
+                else if (curr.right != null) s.push(curr.right);
+            } else if (curr.left == prev) {
+                if (curr.right != null) s.push(curr.right);
+            } else s.pop();
+            prev = curr;
+            depth = Math.max(s.size(), depth);
+        }
+        return depth;
+    }
+
+    //problem 12 find height of a binary tree using Iteration
+    public int heightOfBinaryIteration(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Queue<BinaryTreeNode> s = new LinkedList<>();
+        s.add(root);
+        s.add(null);
+        int depth = 1;
+        while (!s.isEmpty()) {
+            BinaryTreeNode curr = s.poll();
+            if (curr == null) {
+                depth++;
+                s.add(null);
+            } else {
+                if (curr.left == null && curr.right == null) return depth;
+                if (curr.left != null) s.add(curr.left);
+                if (curr.right != null) s.add(curr.right);
+            }
+        }
+        return depth;
+    }
+
+    //problem 13 find the minimum
+    public int minDepth(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Queue<BinaryTreeNode> s = new LinkedList<>();
+        s.add(root);
+        s.add(null);
+        int depth = 1;
+        while (!s.isEmpty()) {
+            BinaryTreeNode curr = s.poll();
+            if (curr == null) {
+                depth++;
+                s.add(null);
+            } else {
+                if (curr.left == null && curr.right == null) return depth;
+                if (curr.left != null) s.add(curr.left);
+                if (curr.right != null) s.add(curr.right);
+            }
+        }
+        return depth;
+    }
+
+    //problem 14: find deepest node of binary tree
+    public BinaryTreeNode deepestNodeInBinaryTreeLevelOrder(BinaryTreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        BinaryTreeNode node = null;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        return node;
+
+
+    }
+
+    //problem 16: find the number of leaf nodes in binary tree without recursion
+    public int countLeavesInBT(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Queue<BinaryTreeNode> q = new LinkedList<>();
+        q.add(root);
+        int count = 0;
+        BinaryTreeNode prev = null;
+        while (!q.isEmpty()) {
+            BinaryTreeNode curr = q.peek();
+            if (curr.left == null && curr.right == null)
+                count++;
+            if (curr.left != null)
+                q.add(curr.left);
+            if (curr.right != null)
+                q.add(curr.right);
+
+        }
+        return count;
+    }
+
+    //problem 17: find the number of full nodes in binary tree
+    public int countFullNodesInBT(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Queue<BinaryTreeNode> q = new LinkedList<>();
+        q.add(root);
+        int count = 0;
+        BinaryTreeNode prev = null;
+        while (!q.isEmpty()) {
+            BinaryTreeNode curr = q.peek();
+            if (curr.left != null && curr.right != null)
+                count++;
+            if (curr.left != null)
+                q.add(curr.left);
+            if (curr.right != null)
+                q.add(curr.right);
+
+        }
+        return count;
+    }
+
+    //problem 18: find the number of half nodes in binary tree without recursion
+    public int countHalfNodesInBT(BinaryTreeNode root) {
+        if (root == null) return 0;
+        Queue<BinaryTreeNode> q = new LinkedList<>();
+        q.add(root);
+        int count = 0;
+        BinaryTreeNode prev = null;
+        while (!q.isEmpty()) {
+            BinaryTreeNode curr = q.peek();
+            if (curr.left == null && curr.right != null || curr.left != null && curr.right == null)
+                count++;
+            if (curr.left != null)
+                q.add(curr.left);
+            if (curr.right != null)
+                q.add(curr.right);
+
+        }
+        return count;
+    }
+
+    //problem 19: given two binary trees check if they are structurally identical
+    public boolean checkStructIdentical(BinaryTreeNode root1, BinaryTreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return checkStructIdentical(root1.left, root2.left) && checkStructIdentical(root1.right, root2.right);
+    }
+
+    //problem 20: find the diameter of binary tree
+    int diameter = 0;
+
+    public int widthOfBinaryTree(BinaryTreeNode root) {
+
+        if (root == null) return 0;
+        int left = widthOfBinaryTree(root.left), right = widthOfBinaryTree(root.right);
+        if (left + right > diameter)
+            diameter = left + right;
+        return Math.max(left, right) + 1;
+
+    }
+
 
 }
