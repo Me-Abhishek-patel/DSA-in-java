5eed31665531f59581f7bb9d55f84534c27c3e20
Prakhar Srivastava <90246139+prakhar2422@users.noreply.github.com>
2021-10-03 14:17:22 +0530

    Update BitProblems.java
    It is decreasing the complexity of code.

--- src/BitProblems.java
+++ src/BitProblems.java
@@ -3,12 +3,12 @@ public class BitProblems {
     //to count set bits in an integer
     //Brian kerningam's algorithm
     public int countSetBits(int num) {
-        int res = 0;
+        int res = 0;//Intialising the value of res
         while (num > 0) {
             num = (num & (num - 1));
-            res++;
+            res++;//Incrementing the value of res
         }
-        return res;
+        return res;//returning the value of res
     }
 
     //to check if a given no. is power of 2 or not
@@ -27,13 +27,13 @@ public class BitProblems {
         for (int i = 0; i < arr.length; i++)
             res = res ^ arr[i];
 
-        return res;
+        return res;//returning the value of res
     }
 
     //to find missing number in n natural numbers
     int missingNum(int arr[]) {
         int n = 0;
-        int i = 0;
+        int i = 0;//initializing the value of i
         while (n <= arr.length + 1) {
             i = i ^ n;
             n++;
