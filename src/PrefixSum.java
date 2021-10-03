f18c2808b4df328b749c433a4a4aa9e03a611204
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2021-10-03 14:27:10 +0530

    Changed activities in main

--- src/PrefixSum.java
+++ src/PrefixSum.java
@@ -6,5 +6,41 @@ public class PrefixSum {
             prefix_sum[i]= prefix_sum[i-1]+arr[i];
         }
         return prefix_sum;
+
+//
+//        BitWise bit = new BitWise();
+//        System.out.println(bit.divideBy2(8));
+//        System.out.println(bit.multiplyBy2(8));
+//        System.out.println(bit.log2(8));
+//
+//        BitProblems bit = new BitProblems();
+//        System.out.println(bit.countSetBits(39));
+
+//        if (bit.isPow2(128))
+//            System.out.println("yes");
+//        else System.out.println("no");
+//
+//        ArrayProblems ap = new ArrayProblems();
+//        int arr[] = {0, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 15, 13};
+//        System.out.println(bit.findOddOccuring(arr));
+
+//        System.out.println(bit.missingNum(arr));
+//        ap.arrayReverse(arr);
+
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//
+//                PrefixSum ps= new PrefixSum();
+//
+//                int arr[] = {2,8,3,9,6,5,4};
+//                int brr[] = ps.prefixSum(arr);
+//                for (int i=0; i<arr.length; i++){
+//                    System.out.print(brr[i]+" ");
+//                }
+
+//            }
+//        }
+
+
     }
 }
