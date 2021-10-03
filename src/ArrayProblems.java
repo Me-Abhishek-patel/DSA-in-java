8e976688022cd8826babc42ec1e72c5acb2c85e3
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2021-10-03 13:11:05 +0530

    Implemented few tactics to solve problems on array, like sliding window technique.

---
+++ src/ArrayProblems.java
@@ -0,0 +1,45 @@
+import java.util.Collection;
+import java.util.Collections;
+
+public class ArrayProblems {
+
+    //to reverse the array
+    void arrayReverse(int arr[]) {
+        int low = 0, high = arr.length - 1, temp = 0;
+        while (low < high) {
+            temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+
+        }
+    }
+
+
+    //to find maximum k consecutive elements
+    //sliding window technique
+    int maxKSum(int arr[],int k){
+        int max=Integer.MIN_VALUE;
+        int win_sum=0;
+        for (int i=0; i<k; i++){
+            win_sum+=arr[i];
+        }
+        max=win_sum;
+
+        for (int i=0; i<arr.length-k; i++){
+            win_sum=win_sum-arr[i]+arr[i+k];
+
+            if (win_sum>max)
+                max=win_sum;
+        }
+        return max;
+    }
+
+    //to find maximum subarray sum
+    //KADANE'S Algorithm
+
+
+}
+
+
