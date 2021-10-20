1211828f9ed4a37d454846994853ef87d5b5106b
Anurag Singh Bhadauria <76035419+Anuraagsinghbhadauria@users.noreply.github.com>
2021-10-20 11:40:05 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -6,12 +6,12 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final double PI = 3.14159;
+        final double PI = 3.14159; 
 
         System.out.println("Enter the value of radius of a circle :");
         double radius = sc.nextDouble();
 
-        double area = (Math.pow(radius,2) * PI);
+        double area = (Math.pow(radius,2) * PI); //formula to calculate area of circle
         System.out.println("The area of a given circle is : "+area);
     }
 }
